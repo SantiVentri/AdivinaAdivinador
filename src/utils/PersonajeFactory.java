822cf95f353d7567c51a1e10425de5b87b7aa037
Santino Ventrice <santino.ventrice@gmail.com>
2026-08-27 00:48:24 -0300

    feat: creando PersonajeFactory sin personajes

---
+++ src/utils/PersonajeFactory.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Personaje;
+
+public class PersonajeFactory {
+	public List<Personaje> crearPersonajes() {
+		List<Personaje> personajes = new ArrayList<Personaje>();
+		
+		// Agregar pesonajes a la lista...
+		
+		return personajes;
+	}
+}
