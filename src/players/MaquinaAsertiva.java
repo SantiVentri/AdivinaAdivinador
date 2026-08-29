b6b195690fb398c238b5f2a86b182e8c552ad562
Santino Ventrice <110856430+SantiVentri@users.noreply.github.com>
2026-08-29 02:19:07 -0300

    feat: agregando método arriesgarPersnaje() para MaquinaAleatoria

--- src/players/MaquinaAsertiva.java
+++ src/players/MaquinaAsertiva.java
@@ -1,10 +1,15 @@
 package players;
 
+import java.util.List;
+import java.util.Random;
+
 import model.FiltroAplicado;
 import model.Personaje;
 import model.Tablero;
 
 public class MaquinaAsertiva extends Jugador {
+	private final Random random = new Random();
+	
 	public MaquinaAsertiva(Tablero tablero) {
 		super("Máquina Asertiva", tablero);
 		// TODO Auto-generated constructor stub
@@ -18,8 +23,15 @@ public class MaquinaAsertiva extends Jugador {
 
 	@Override
 	public Personaje arriesgarPersonaje() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Personaje> restantes = getTablero().getPersonajesRestantes();
+
+        if (restantes.isEmpty()) {
+            return null;
+        }
+
+        int indiceRandom = random.nextInt(restantes.size());
+        Personaje personajeElegido = restantes.get(indiceRandom);
+        return personajeElegido;
 	}
 
 }
